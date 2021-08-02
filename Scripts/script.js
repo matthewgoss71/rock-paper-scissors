e02d8dd478cf74e8edc68a65b1d4544636771ba8
matthewgoss71 <matthewgoss71@gmail.com>
2021-08-02 18:29:08 -0500

    added player selection

--- Scripts/script.js
+++ Scripts/script.js
@@ -3,3 +3,12 @@ function computerPlay() {
     let computerChoice = possibleOutcomes[Math.floor(Math.random()*possibleOutcomes.length)]
     return computerChoice;
 }
+
+//Player inputs selection
+
+let playerSelection = prompt('Select Rock, Paper, or Scissors!', '');
+//Computer makes selection of its own
+//Computer compares both selections
+//Computer determines outcome
+//Computer alerts player results
+//Game restarts
