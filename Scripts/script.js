9aaf74b7ce6069e63223727acb2e2265336e65dc
matthewgoss71 <matthewgoss71@gmail.com>
2021-08-02 18:39:54 -0500

    first attempt at actual gameplay

--- Scripts/script.js
+++ Scripts/script.js
@@ -1,14 +1,29 @@
 function computerPlay() {
     let possibleOutcomes = ["Rock", "Paper", "Scissors"]
-    let computerChoice = possibleOutcomes[Math.floor(Math.random()*possibleOutcomes.length)]
-    return computerChoice;
+    let computerSelection = possibleOutcomes[Math.floor(Math.random()*possibleOutcomes.length)]
+    return computerSelection;
 }
 
 //Player inputs selection
 
 let playerSelection = prompt('Select Rock, Paper, or Scissors!', '');
+
 //Computer makes selection of its own
+
+computerPlay();
+
 //Computer compares both selections
+
+function gamePlay(playerSelection, computerPlay()) {
+    if (playerSelection === computerPlay()) {
+        alert("It's a draw!");
+    }   else if ((playerSelection === 'Rock' && computerPlay === 'Scissors') || (playerSelection === 'Paper' && computerPlay === 'Rock') || (playerSelection === 'Scissors' && computerPlay === 'Paper')) {
+        alert("Congratulations! You have beaten the computer!");
+    }   else if ((playerSelection === 'Rock' && computerPlay === 'Paper') || (playerSelection === 'Paper' && computerPlay == 'Scissors') || (playerSelection === 'Scissors' && computerPlay === 'Rock')) {
+        alert("Better luck next time, the computer has defeated you!");
+    }   else (playerSelection === 'null' || 'undefined') {
+        alert("You have to actually try to win!");
+    }
+}
 //Computer determines outcome
 //Computer alerts player results
-//Game restarts
