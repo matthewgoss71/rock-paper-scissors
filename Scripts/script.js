0ff789d9bdd638b6407de0d8d19a36eb69a833e2
matthewgoss71 <matthewgoss71@gmail.com>
2021-08-02 21:46:44 -0500

    changed variables to more descriptive names, added comments for clarification

--- Scripts/script.js
+++ Scripts/script.js
@@ -1,49 +1,53 @@
+//Function chooses random selection from r, p, and s
+
 function computerPlay() {
     let possibleOutcomes = ["rock", "paper", "scissors"]
     let computerSelection = possibleOutcomes[Math.floor(Math.random()*possibleOutcomes.length)]
     return computerSelection;
 }
 
-let x = 0;
-let y = 0;
+//Assign variables for scoring
+
+let playerScore = 0;
+let computerScore = 0;
+
+//Single round gameplay function
 
 function playRound() {
 
     let playerSelection = prompt('Select rock, paper, or scissors!', '');
     let playerSelectionLower = playerSelection.toLowerCase();
-    //console.log('Player chooses ' + playerSelectionLower);
     let computerSelection = computerPlay();
-    //console.log('Computer chooses ' + computerSelection);
 
     if (playerSelectionLower === computerSelection) {
         alert("It's a draw!");
-        return (x + 0);
     }   else if ((playerSelectionLower === 'rock' &&  computerSelection === 'scissors') || (playerSelectionLower === 'paper' && computerSelection === 'rock') || (playerSelectionLower === 'scissors' && computerSelection === 'paper')) {
         alert("Congratulations! You have beaten the computer!");
-        console.log('Player ' + ++x + ' ' + 'Computer ' + y);
-        return (x);
+        console.log('Player ' + ++playerScore + ' ' + 'Computer ' + computerScore);
     }   else if ((playerSelectionLower === 'rock' && computerSelection === 'paper') || (playerSelectionLower === 'paper' && computerSelection == 'scissors') || (playerSelectionLower === 'scissors' && computerSelection === 'rock')) {
         alert("Better luck next time, the computer has defeated you!");
-        console.log('Player ' + x + ' ' + 'Computer ' + ++y);
-        return (y);
+        console.log('Player ' + playerScore + ' ' + 'Computer ' + ++computerScore);
     }   else {
-        alert("You have to actually try to win!");
-        return (y + 0);
+        alert("Quitting already? Don't give up just yet!");
     }
 }
 
+//Best of five function
+
 function game() {
-   while ((x >= 0 && x <= 5) && (y >= 0 && y <= 5)) {
-       if (x === 5) {   
+   while ((playerScore >= 0 && playerScore <= 5) && (computerScore >= 0 && computerScore <= 5)) {
+       if (playerScore === 5) {   
            alert('Congrats! You Have topped the computer in a best of five rock, paper, scissors match!');
            break;
-    }  else if (y === 5) {
+    }  else if (computerScore === 5) {
            alert('Better luck next time, the computer has topped you in a best of five rock, paper, scissors match!');
            break;
     }
         playRound();
    }
 } 
+
+//Run the game
 
 game();
 
