7a81c239b1e8c260fbb0c0020211ee2ee2597f73
matthewgoss71 <matthewgoss71@gmail.com>
2021-08-02 19:38:07 -0500

    fixed tolowercase and removed erroneous code

--- Scripts/script.js
+++ Scripts/script.js
@@ -1,33 +1,32 @@
 function computerPlay() {
-    let possibleOutcomes = ["Rock", "Paper", "Scissors"]
+    let possibleOutcomes = ["rock", "paper", "scissors"]
     let computerSelection = possibleOutcomes[Math.floor(Math.random()*possibleOutcomes.length)]
     return computerSelection;
 }
 
 //Player inputs selection
 
-let playerSelection = prompt('Select Rock, Paper, or Scissors!', '');
+let playerSelection = prompt('Select rock, paper, or scissors!', '');
 
 //Computer makes selection of its own
 
 let computerSelection = computerPlay();
-console.log(computerSelection);
 
 //Convert all selections to lowercase for comparison
 
 let playerSelectionLower = playerSelection.toLowerCase();
-let computerSelectionLower = computerSelection.toLowerCase();
+console.log(playerSelectionLower);
 
 //Computer compares both selections
 //Computer determines outcome
 //Computer alerts player results
 
 function gamePlay() {
-    if (playerselectionLower === computerSelectionLower) {
+    if (playerSelectionLower === computerSelection) {
         alert("It's a draw!");
-    }   else if ((playerselectionLower === 'Rock' &&  computerSelectionLower === 'Scissors') || (playerselectionLower === 'Paper' && computerSelectionLower === 'Rock') || (playerselectionLower === 'Scissors' && computerSelectionLower === 'Paper')) {
+    }   else if ((playerSelectionLower === 'rock' &&  computerSelection === 'scissors') || (playerSelectionLower === 'paper' && computerSelection === 'rock') || (playerSelectionLower === 'scissors' && computerSelection === 'paper')) {
         alert("Congratulations! You have beaten the computer!");
-    }   else if ((playerselectionLower === 'Rock' && computerSelectionLower === 'Paper') || (playerselectionLower === 'Paper' && computerSelectionLower == 'Scissors') || (playerselectionLower === 'Scissors' && computerSelectionLower === 'Rock')) {
+    }   else if ((playerSelectionLower === 'rock' && computerSelection === 'paper') || (playerSelectionLower === 'paper' && computerSelection == 'scissors') || (playerSelectionLower === 'scissors' && computerSelection === 'rock')) {
         alert("Better luck next time, the computer has defeated you!");
     }   else {
         alert("You have to actually try to win!");
