147bfe50b2dfffe69187fa0a7d9668d7eb03d824
matthewgoss71 <matthewgoss71@gmail.com>
2021-08-02 19:30:02 -0500

    added case insensitivity

--- Scripts/script.js
+++ Scripts/script.js
@@ -10,20 +10,29 @@ let playerSelection = prompt('Select Rock, Paper, or Scissors!', '');
 
 //Computer makes selection of its own
 
-computerPlay();
+let computerSelection = computerPlay();
+console.log(computerSelection);
+
+//Convert all selections to lowercase for comparison
+
+let playerSelectionLower = playerSelection.toLowerCase();
+let computerSelectionLower = computerSelection.toLowerCase();
 
 //Computer compares both selections
+//Computer determines outcome
+//Computer alerts player results
 
-function gamePlay(playerSelection, computerPlay()) {
-    if (playerSelection === computerPlay()) {
+function gamePlay() {
+    if (playerselectionLower === computerSelectionLower) {
         alert("It's a draw!");
-    }   else if ((playerSelection === 'Rock' && computerPlay === 'Scissors') || (playerSelection === 'Paper' && computerPlay === 'Rock') || (playerSelection === 'Scissors' && computerPlay === 'Paper')) {
+    }   else if ((playerselectionLower === 'Rock' &&  computerSelectionLower === 'Scissors') || (playerselectionLower === 'Paper' && computerSelectionLower === 'Rock') || (playerselectionLower === 'Scissors' && computerSelectionLower === 'Paper')) {
         alert("Congratulations! You have beaten the computer!");
-    }   else if ((playerSelection === 'Rock' && computerPlay === 'Paper') || (playerSelection === 'Paper' && computerPlay == 'Scissors') || (playerSelection === 'Scissors' && computerPlay === 'Rock')) {
+    }   else if ((playerselectionLower === 'Rock' && computerSelectionLower === 'Paper') || (playerselectionLower === 'Paper' && computerSelectionLower == 'Scissors') || (playerselectionLower === 'Scissors' && computerSelectionLower === 'Rock')) {
         alert("Better luck next time, the computer has defeated you!");
-    }   else (playerSelection === 'null' || 'undefined') {
+    }   else {
         alert("You have to actually try to win!");
     }
 }
-//Computer determines outcome
-//Computer alerts player results
+
+gamePlay();
+
