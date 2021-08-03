c7eaf00d14d5f2a30daa57e993e888d466903ea5
matthewgoss71 <matthewgoss71@gmail.com>
2021-08-02 19:58:54 -0500

    added score counter and 5 game run

--- Scripts/script.js
+++ Scripts/script.js
@@ -4,35 +4,36 @@ function computerPlay() {
     return computerSelection;
 }
 
-//Player inputs selection
+function playRound() {
 
-let playerSelection = prompt('Select rock, paper, or scissors!', '');
+    let playerSelection = prompt('Select rock, paper, or scissors!', '');
+    let playerSelectionLower = playerSelection.toLowerCase();
+    //console.log('Player chooses ' + playerSelectionLower);
+    let computerSelection = computerPlay();
+    //console.log('Computer chooses ' + computerSelection);
+    let x = 0;
+    let y = 0;
 
-//Computer makes selection of its own
-
-let computerSelection = computerPlay();
-console.log('Computer chooses ' + computerSelection);
-
-//Convert all selections to lowercase for comparison
-
-let playerSelectionLower = playerSelection.toLowerCase();
-console.log('Player chooses ' + playerSelectionLower);
-
-//Computer compares both selections
-//Computer determines outcome
-//Computer alerts player results
-
-function gamePlay() {
     if (playerSelectionLower === computerSelection) {
         alert("It's a draw!");
     }   else if ((playerSelectionLower === 'rock' &&  computerSelection === 'scissors') || (playerSelectionLower === 'paper' && computerSelection === 'rock') || (playerSelectionLower === 'scissors' && computerSelection === 'paper')) {
         alert("Congratulations! You have beaten the computer!");
+        console.log('Player ' + ++x);
     }   else if ((playerSelectionLower === 'rock' && computerSelection === 'paper') || (playerSelectionLower === 'paper' && computerSelection == 'scissors') || (playerSelectionLower === 'scissors' && computerSelection === 'rock')) {
         alert("Better luck next time, the computer has defeated you!");
+        console.log('Computer ' + ++y);
     }   else {
         alert("You have to actually try to win!");
     }
 }
 
-gamePlay();
+function game() {
+    playRound();
+    playRound();
+    playRound();
+    playRound();
+    playRound();
+}
+
+game();
 
